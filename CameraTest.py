0a2f658bd1012cbb162f69416d88e3e0b9c694bb
TheCanMoun10 <58854102+TheCanMoun10@users.noreply.github.com>
2020-10-15 10:52:41 +0200

    Update CameraTest.py

--- CameraTest.py
+++ CameraTest.py
@@ -1,34 +1,5 @@
 import cv2.cv2 as cv2
 import threading
-import imutils
-from imutils.video import VideoStream
-
-class Stitcher:
-    def __init__(self):
-        # Determine the version of opencv we are using, and initialize the homography matrix.
-        self.isv3 = imutils.is_cv3()
-        self.cachedH = None
-
-    def stitch(self, images, ratio=0.75, reprojThresh=4.0):
-        #Unpacking images:
-        (imageB, imageA) = images
-        if self.cachedH is None:
-            #If the homography matrix is None, we need to apply keypoint matching for contructing.
-            (kpsA, featuresA) = self.detectAndDescribe(imageA)
-            (kpsB, featuresB) = self.detectAndDescribe(imageB)
-
-            #Match features between two images
-            M = self.matchKeypoints(kpsA, kpsB,
-			featuresA, featuresB, ratio, reprojThresh)
-
-            if M is None:
-                return None
-
-            self.cachedH = M[1]
-        result = cv2.warpPerspective(imageA, self.cachedH, (imageA.shape[1] + imageB.shape[1], imageA.shape[0]))
-        result[0:imageB.shape[0], 0:imageB.shape[1]] = imageB
-        return result
-
 
 class camThread(threading.Thread):
     def __init__(self, previewName, camID):
