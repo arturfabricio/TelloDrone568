9d534dae6a6af4a5b8953ffd3a161ab4a559e3eb
Artur Fabrício <45845054+arturfabricio@users.noreply.github.com>
2020-11-10 15:57:50 +0100

    Add files via upload

--- Drone+Processing.py
+++ Drone+Processing.py
@@ -75,52 +75,46 @@ def videoDisplay():
     global dilation
     global box
     while stream_state:
+        forward(30)
         #New portion Image Processing
         scale_percent = 70  # percent of original size
         width = int(frame.shape[1] * scale_percent / 100)
-        # print(width) 
         height = int(frame.shape[0] * scale_percent / 100)
-        # print(height)
-        #print(width*height)
         dim = (width, height)
-        # resize image
+        # print(height)
+        # print(width)
+        xreal = int(width/2)
+        yreal = int(height/2)
         resized = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+        final = np.copy(resized)
+
         #Center Point
-        xreal = width/2
-        yreal = height/2
-        cX_IF = 0
-        cY_IF = 0
-        center_coordinates = (int(xreal),int(yreal))
-        radius = 3
+        center_coordinates = (xreal,yreal)
+        radius = 2
         color = (0,0,255)
         thickness = -1
         cv2.circle(resized,center_coordinates,radius,color,thickness)
-        cv2.imshow('original', resized)
-        cv2.waitKey(1)
-
-        #############################################################
-        ########## OBSTACLE AVOIDANCE ###############################
-        #############################################################
+        #cv2.imshow('original', resized)
+        
+        CandB = np.zeros(resized.shape, resized.dtype)
+        alpha = 1
+        beta = 50
+        CandB = cv2.convertScaleAbs(resized, alpha=alpha, beta=beta)
+        cv2.imshow('Constrast and Brightness', CandB)
 
         #GaussianBlur
-        blur = cv2.GaussianBlur(resized, (11, 11), 0)
-        # cv2.imshow('Gaussian Blur', blur)
-        # cv2.waitKey(1)
+        blur = cv2.GaussianBlur(CandB, (3, 3), 0)
+        #cv2.imshow('Gaussian Blur', blur)
 
         #HSV
         HSV = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-        # cv2.imshow('HSV', HSV)
-        # cv2.waitKey(1)
+        #cv2.imshow('HSV', HSV)
 
         #Mask
-        #Hue -> [0,179]
-        #Saturation -> [0,255]
-        #Value -> [0,255]
-        threshold1 = (0, 0, 0)
-        threshold2 = (110, 110, 100)
-        mask = cv2.inRange(HSV, threshold1, threshold2)
-        # cv2.imshow('mask', mask)
-        # cv2.waitKey(1)
+        light_orange = (0, 0, 0)
+        dark_orange = (110, 110, 100)
+        mask = cv2.inRange(HSV, light_orange, dark_orange)
+        #cv2.imshow('mask', mask)
 
         # Specify size on vertical axis
         vertical = np.copy(mask)
@@ -132,126 +126,102 @@ def videoDisplay():
         vertical = cv2.erode(vertical, verticalStructure)
         vertical = cv2.dilate(vertical, verticalStructure)
         # Show extracted vertical lines
-        # cv2.imshow("vertical", vertical)
-        # cv2.waitKey(1)
+        #cv2.imshow("vertical", vertical)
 
         #Opening
-        kernel = np.ones((15,15),np.uint8)
+        kernel = np.ones((17,17),np.uint8)
         opening = cv2.morphologyEx(vertical, cv2.MORPH_OPEN, kernel)
-        # cv2.imshow('opening', opening)
-        # cv2.waitKey(1)
+        #cv2.imshow('opening', opening)
 
-        #Erosion
-        kernel1 = np.ones((3,19),np.uint8)
+        kernel1 = np.ones((3,10),np.uint8)
         erosion = cv2.morphologyEx(vertical, cv2.MORPH_ERODE, kernel1)
-        # cv2.imshow('erosion', erosion)
-        # cv2.waitKey(1)
+        #cv2.imshow('erosion', erosion)
 
         #New portion of code for BLOB detection and line drawing
         kernel2 = np.ones((55,35),np.uint8)
         dilation = cv2.morphologyEx(erosion, cv2.MORPH_DILATE, kernel2)
-        # cv2.imshow('dilation', dilation)
-        # cv2.waitKey(1)
+        #cv2.imshow('dilation',dilation)
 
-        #contours[0] = 0
         contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        if len(contours) > 0:
-            cnt = contours[0]
-            M = cv2.moments(cnt)
-            area = cv2.contourArea(cnt)
+        for c in contours:
+            M = cv2.moments(c)
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            area = cv2.contourArea(c)
+            rect = cv2.boundingRect(c)
+
+            areaList = area
+            xList = rect[0]
+            yList = rect[1] 
+            wList = rect[2]        
+            hList = rect[3]
 
             dilation2 = cv2.cvtColor(dilation, cv2.COLOR_GRAY2BGR)
+            cv2.drawContours(dilation2, contours, -1, (0, 255, 0), 3) 
+            cv2.drawContours(resized, contours, -1, (0, 255, 0), 3) 
 
-            for c in contours:
-                box = np.zeros((504,672,3),np.uint8)
-                rect = cv2.boundingRect(c)
-                height = rect[3]
-                #print(height)
-                area = cv2.contourArea(c)
-                x,y,w,h = rect
-                cX = int(x+(w/2))
-                cY = int(y+(h/2))
-                dist = int(((xreal-cX)**2 + (yreal-cY)**2)**.5)
-                #print(dist)
-                # radius = int(200)
-                # cv2.circle(box,center_coordinates,radius,(0,255,0),2)
-                cv2.circle(box,center_coordinates,radius,color,thickness)
-                #print(area)
-                decision = False
-                if rect[3] > 50 and area < 60000.0:
-                    if dist < 300:
-                        #print(rect)
-                        #print(height)
-                        print(" ")
-                        cX_IF = int(rect[0] + (rect[2]/2))
-                        print(cX_IF)
-                        cY_IF = int(rect[1] + (rect[3]/2))
-                        print(cY_IF)
-                        new_coordinates = (int(cX_IF),int(cY_IF))
-                        cv2.rectangle(dilation2,(x,y),(x+w,y+h),(0,255,0),2)
-                        cv2.circle(box,center_coordinates,radius,(0,255,0),2)
-                        cv2.putText(dilation2,"YES",(x+w+10,y+h),0,0.3,(0,255,0))
-                        cv2.rectangle(box,(x,y),(x+w,y+h),(0,255,0),2)
-                        cv2.putText(box,"YES",(x+w+10,y+h),0,1,(0,255,0))
-                        cv2.arrowedLine(box, center_coordinates, (cX_IF, cY_IF), (0,0,255), 1)
-                        decision = True
+            if hList > 400:
+                print("im in!")
+                start_point = (xList, yList) 
+                end_point = (xList+wList, yList+hList) 
+                color = (255, 0, 0)  
+                thickness = -1
+                image = cv2.rectangle(final, start_point, end_point, color, thickness)
 
-                        #print(int(width/2))
-                        #print(int(height/2))
+                if xList < int(width/2):
+                    quadrant = "left"
+                    print("This blob belongs to the " + quadrant + " side.")                
+                elif xList > int(width/2):
+                    quadrant = "right"
+                    print("This blob belongs to the " + quadrant + " side.")                
+                    
+            cv2.imshow('Final', final)
+            cv2.imshow('Original', resized)
+            i += 1
+        cv2.waitKey(20)
 
-                        if cX_IF < int(width/2) and cY_IF < int(height/2):
-                            quadrant = 1
-                            print("This point belongs to quadrant number " + str(quadrant))                
-                        
-                        elif int(width/2) < cX_IF  and cX_IF < int(width) and cY_IF < int(height/2):
-                            quadrant = 2
-                            print("This point belongs to quadrant number " + str(quadrant))
+    if quadrant == "left":
+        takeoff()
+        forward(0)
+        print("forward")
+        time.sleep(4)
+        turn(30)
+        print("first turn")
+        time.sleep(4)
+        forward(20)
+        print("forward")
+        time.sleep(4)
+        turn(-30)
+        print("second turn")
+        time.sleep(4)
+        forward(20)
+        print("forward")
+        time.sleep(5)
+        forward(0)
+        land()
+        print("land")
 
-                        elif cX_IF < int(width/2) and int(height/2) < cY_IF and cY_IF < height:
-                            quadrant = 3
-                            print("This point belongs to quadrant number " + str(quadrant))
-
-                        elif int(width/2) < cX_IF and cX_IF < width and int(height/2) < cY_IF and cY_IF < height:
-                            quadrant = 4
-                            print("This point belongs to quadrant number " + str(quadrant))
-                else:
-                    decision = False
-                    break           
-        else:
-            print("Sorry No contour Found.")
-    
-
-        decision1 = input()
-
-        if decision1 == True:
-            forward(0)
-            print("forward")
-            time.sleep(1000)
-            turn(-30)
-            print("first turn")
-            time.sleep(5000)
-            forward(20)
-            print("forward")
-            time.sleep(5000)
-            turn(30)
-            print("second turn")
-            time.sleep(5000)
-            forward(20)
-            print("forward")
-            time.sleep(2500)
-            forward(0)
-            land()
-            print("land")
-
-
-        cv2.imshow("Show", dilation2)
-        cv2.waitKey(1)
-        cv2.imshow("box", box)
-        cv2.waitKey(1)
-
-            
-
-        
+    if quadrant == "right":
+        takeoff()
+        forward(0)
+        print("forward")
+        time.sleep(4)
+        turn(-30)
+        print("first turn")
+        time.sleep(2)
+        forward(20)
+        print("forward")
+        time.sleep(3)
+        turn(30)
+        print("second turn")
+        time.sleep(2)
+        forward(20)
+        print("forward")
+        time.sleep(2.5)
+        forward(0)
+        land()
+        print("land")
+      
 def battery():
     global current_time
     while True:
@@ -277,89 +247,6 @@ def commands():
                 break
             msg = msg.encode(encoding="utf-8")
             sent = sock.sendto(msg, command_address)
-
-takeoff()
-commands()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            # #############################################################
-        # #########WALL AVOIDANCE######################################
-        # #############################################################
-
-        # #Median Blur
-        # medianBlur = cv2.medianBlur(resized, 45)
-        # # cv2.imshow('Median Blur', medianBlur)
-        # # cv2.waitKey(1)
-
-        # #Gresycale
-        # grey_scale = cv2.cvtColor(medianBlur, cv2.COLOR_BGR2GRAY)
-        # # cv2.imshow('Grey Scale', grey_scale)
-        # # cv2.waitKey(1)
-
-        # #Treshold
-        # ret,thresh1 = cv2.threshold(grey_scale,127,255,cv2.THRESH_BINARY)
-        # # cv2.imshow('Treshold', thresh1)
-        # # cv2.waitKey(1)
-
-        # # Closing
-        # closekernel = np.ones((15,15),np.uint8)
-        # closing = cv2.morphologyEx(thresh1, cv2.MORPH_CLOSE, closekernel)
-        # #cv2.imshow('Closing', closing)
-        # #cv2.waitKey(1)
-
-        # # Blob Data
-        # n, labels, stats, _ = cv2.connectedComponentsWithStats(closing)
-
-        # area = stats[labels,cv2.CC_STAT_AREA]
-        # #print(area[0][0])
-
-
-
-
-        #     while stream_state:
-        # # cv2.imshow('resized', resized)
-        # # cv2.waitKey(1)
-        # cv2.imshow('dilation', dilation)
-        # cv2.waitKey(1)
-
-        # #BLOB detector
-        # params = cv2.SimpleBlobDetector_Params()
-
-        # params.filterByArea = True
-        # params.minArea = 300
-        # params.maxArea = 338688
-        # params.filterByColor = True
-        # params.blobColor = 255
-
-        # ver = (cv2.__version__).split('.')
-        # if int(ver[0]) < 3 :
-        #     detector = cv2.SimpleBlobDetector(params)
-        # else: 
-        #     detector = cv2.SimpleBlobDetector_create(params)
-
-        # keypoints = detector.detect(dilation)
-        # print(keypoints)
-        # im_with_keypoints = cv2.drawKeypoints(dilation, keypoints, np.array([]), (0,0,255), cv2.DFT_REAL_OUTPUT)
-
-        # cv2.imshow("final",im_with_keypoints)       
+#takeoff()
+time.sleep(10)
+commands()   
