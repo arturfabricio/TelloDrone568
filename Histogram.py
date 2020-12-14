b1ec25677d06b351cc5876bf39d40971c05d1505
Artur Fabrício <45845054+arturfabricio@users.noreply.github.com>
2020-12-14 11:47:10 +0100

    Add files via upload

--- Histogram.py
+++ Histogram.py
@@ -7,7 +7,7 @@ import matplotlib.colors as colors
 import numpy as np
 from scipy import misc
 
-img = imageio.imread("5.png")
+img = imageio.imread("1.png")
 array=np.asarray(img)
 arr=(array.astype(float))/255.0
 img_hsv = colors.rgb_to_hsv(arr[...,:3])
@@ -18,8 +18,10 @@ plt.hist(lu1*360,bins=360,range=(0.0,360.0),histtype='stepfilled', color='r', la
 plt.title("Hue")
 plt.xlabel("Value")
 plt.ylabel("Frequency")
-plt.axvline(0, color='black',linewidth=2)
-plt.axvline(110, color='black',linewidth=2)
+plt.axvline(0, color='black',linewidth=3)
+plt.axvline(110, color='black',linewidth=3)
+axes = plt.gca()
+axes.set_ylim([0,30000])
 plt.legend()
 
 lu2=img_hsv[...,1].flatten()
@@ -28,8 +30,10 @@ plt.hist(lu2,bins=100,range=(0.0,1.0),histtype='stepfilled', color='g', label='S
 plt.title("Saturation")   
 plt.xlabel("Value")    
 plt.ylabel("Frequency")
-plt.axvline(0, color='black',linewidth=2)
-plt.axvline(0.43, color='black',linewidth=2)
+plt.axvline(0, color='black',linewidth=3)
+plt.axvline(0.43, color='black',linewidth=3)
+axes = plt.gca()
+axes.set_ylim([0,15000])
 plt.legend()
 
 lu3=img_hsv[...,2].flatten()
@@ -38,60 +42,9 @@ plt.hist(lu3*255,bins=256,range=(0.0,255.0),histtype='stepfilled', color='b', la
 plt.title("Intensity")   
 plt.xlabel("Value")    
 plt.ylabel("Frequency")
-plt.axvline(0, color='black',linewidth=2)
-plt.axvline(110, color='black',linewidth=2)
+plt.axvline(0, color='black',linewidth=3)
+plt.axvline(110, color='black',linewidth=3)
+axes = plt.gca()
+axes.set_ylim([0,8000])
 plt.legend()
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import numpy as np
-# import cv2 as cv
-# from matplotlib import pyplot as plt
-
-# img = cv.imread('2.png')
-# hsv = cv.cvtColor(img,cv.COLOR_BGR2HSV)
-
-# hist = cv.calcHist( [hsv], [0, 1], None, [180, 256], [0, 180, 0, 256] )
-# # Add title and axis names
-# plt.title('HSV Histogram')
-# plt.xlabel('Saturation Values')
-# plt.ylabel('Hue Values')
-# plt.imshow(hist,interpolation = 'nearest')
-# plt.show()
